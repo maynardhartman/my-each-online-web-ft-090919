fb3fc5dd7d00c0d8b4104ddcc82e0b7482904e0d
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-15 04:57:06 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,14 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
+  i = 0
   # code here
+  while i < array.length 
+    yield array[i]
+    i = i + 1
+  end
+  puts "end"
+  array
+end
+
+my_each do |number|
+  puts number
 end
