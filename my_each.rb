36a7f5d2de04c6da739e4f85790611d0577424d3
Maynard Hartman <mjhartman@brighthouse.com>
2019-09-15 02:00:10 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
+def my_each(array) # put argument(s) here
+  i = 0
   # code here
+  while i < array.length 
+    yield array[i]
+  end
+end
+collection = [1, 2, 3, 4]
+my_each(collection) do |number|
+  puts number
 end
